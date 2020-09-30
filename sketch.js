dc6273930b752d77175a8b3a779c7f23256bb440
Sohail-Rasul <69708396+Sohail-Rasul@users.noreply.github.com>
2020-09-30 19:45:47 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,34 +2,65 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Render = Matter.Render;
 
-var ground;
-var divisionHeight=300;
 var particles=[];
 var plinkos=[];
 var divisions=[];
+var divH=300;
+var ground;
 
 function setup() {
-  createCanvas(480,800);
+  createCanvas(480,700);
 
   engine = Engine.create();
-  world = engine.world; 
-
-  ground=new Ground(240,785,480,30);
+  world = engine.world;
   
+  for(var k=0;k<=width;k=k+80)
+  {
+    divisions.push(new Division(k,height-divH/2,10,divH));
+  }
+  for(var j=40;j<=width;j=j+50)
+  {
+    plinkos.push(new Plinko(j,75));
+  }
+  for(var j=15;j<=width;j=j+50)
+  {
+    plinkos.push(new Plinko(j,175));
+  }
+  for(var j=40;j<=width;j=j+50)
+  {
+    plinkos.push(new Plinko(j,275));
+  }
+ 
+
+  
+  
+  ground=new Ground(width/2,height,width,10);
+   
   Engine.run(engine);
+  //createSprite(400, 200, 50, 50);
 }
 
 function draw() {
-  background(0); 
-  for(var i=0; i<=innerWidth; i+80){
-    divisions.push(new Division(i,height-divisionHeight/2,10,divisionHeight));
-  } 
+  background('black');  
+  ground.display();
   if(frameCount%60===0){
-    particles.push(new Particle(random(width/2-10,width/2+10),10,10))
+    particles.push(new Particle(random(width/2-10,width/2+10),10,10));
+  }
+  console.log(frameCount);
+  for(var j=0;j<particles.length;j++)
+  {
+    particles[j].display();
+  }
+  for(var k=0;k<divisions.length;k++)
+  {
+    divisions[k].display();
+  }
+  for(var i=0;i<plinkos.length;i++)
+  {
+    plinkos[i].display();
   }
   
- ground.display();
- }
- 
- 
+  drawSprites();
+}
